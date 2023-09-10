f9dbe0376b4bdbdf44e07a4281e6e95e2d119e69
MarkuSchick <markuschick@gmail.com>
2023-09-10 19:31:52 +0100

    feat: perform call to precompile

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use ethers::prelude::*;
 use rayon::prelude::*;
 use sha2::Digest;
 
+const USER_ADDRESS: &str = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"; // change this to your address
+const SHA256_ADDRESS: &str = "0x0000000000000000000000000000000000000002";
+const RPC_URI: &str = "https://eth.llamarpc.com";
+
 // Generates the binding `SolveThePuzzleOfCoastWithImpressionInNightAndSquallOnCayAndEndToVictoryCall`
 // Need to run `forge build` before `cargo build`.
 abigen!(IKey, "./out/IKey.sol/IKey.json");
@@ -26,10 +30,6 @@ fn abi_encode(owner: H160, password: [u8; 32]) -> Vec<u8> {
         .encode()
 }
 
-const SHA256_ADDRESS: &str = "0x0000000000000000000000000000000000000002";
-const USER_ADDRESS: &str = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"; // change this to your address
-const RPC_URI: &str = "https://eth.llamarpc.com";
-
 fn get_client() -> Arc<Provider<Http>> {
     let provider: Provider<Http> = Provider::<Http>::try_from(RPC_URI).expect("Valid URL");
     Arc::new(provider)
@@ -45,7 +45,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let owner: Address = USER_ADDRESS.parse().expect("Valid address");
 
     // 0x00000000
-    let sig = hex::decode("00").unwrap();
+    let sig = hex::decode("00000000").unwrap();
     let res = (0..u128::MAX).into_par_iter().find_any(|i| {
         let password = to_password(*i);
         let digest = sha2::Sha256::digest(&abi_encode(owner, password));
@@ -57,7 +57,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let password = to_password(i);
         println!("password: {:#?}", password);
 
-        // make an staticcall to the ethereum json rpc address and address with the abi encoding
+        // perform an test call to keccak address
         let client = get_client();
         let hash_address = get_address_two(&client);
         let res = hash_address.solve_the_puzzle_of_coast_with_impression_in_night_and_squall_on_cay_and_end_to_victory(owner, password).call().await.unwrap();
